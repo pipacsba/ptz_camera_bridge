a2c83860d848958960f8d9448f02bf0550292c0d
pipacsba <44190358+pipacsba@users.noreply.github.com>
2026-07-28 11:35:30 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ def setup_logging():
     cannot override our logging configuration.
     """
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format="%(asctime)s %(levelname)-8s %(name)s: %(message)s",
         force=True,
     )
