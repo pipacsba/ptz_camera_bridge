fb36c728b824b013b962e958e43a2928b386766d
pipacsba <44190358+pipacsba@users.noreply.github.com>
2026-07-28 12:56:27 +0200

    Update discovery.py

--- src/discovery.py
+++ src/discovery.py
@@ -43,7 +43,7 @@ class DiscoveryPublisher:
             camera: Camera implementation.
         """
         device = {
-            "identifiers": [ "frigate","59ed0c90be0f836074675f3fb7156450" ],
+            "identifiers": [ "frigate","01K8D5KB1VBH56CZAK0JHTT294:sonoff_pt2" ],
         }
         
         log.info(
